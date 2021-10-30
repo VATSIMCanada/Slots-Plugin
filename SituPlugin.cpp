3666fde327b00133ba29fdae08e5830dadcb4aeb
ronyan <r.yan163@gmail.com>
2021-10-29 21:53:45 -0400

    version num

--- SituPlugin.cpp
+++ SituPlugin.cpp
@@ -20,7 +20,7 @@ const int TAG_ITEM_NAT_EXTRA = 5009;
 SituPlugin::SituPlugin()
 	: EuroScopePlugIn::CPlugIn(EuroScopePlugIn::COMPATIBILITY_CODE,
 		"VATCAN Bookings",
-		"1.1.1-amendfp",
+		"1.1.2-amendfp",
 		"VATCAN",
 		"Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)")
 {
